5be0a0ba2013f6d9f463acdf839729a25e9e0a79
Duong Do <dohoangduong95@gmail.com>
2017-01-12 17:26:25 -0500

    Add view and urk for book detail

--- textbook/urls.py
+++ textbook/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='index'),
+    url(r'book_detail/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book_detail'),
 ]
--- textbook/views.py
+++ textbook/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, DetailView
+from .models import Book, Section, BookSection
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
