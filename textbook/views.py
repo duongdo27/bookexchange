f0edc4a4e62f1d4d3f6e96b7493cabc7f6cf6e45
Duong Do <dohoangduong95@gmail.com>
2017-01-24 22:06:18 -0600

    Add userbook with detail

--- textbook/views.py
+++ textbook/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import TemplateView, DetailView, ListView, UpdateView
 from .models import Book, Section, BookSection, UserBook
 from .form import UserBookForm
-from django.shortcuts import redirect
 import ipdb
 
 
@@ -23,11 +22,15 @@ class BookDetailView(DetailView):
         section_data = [x.section.__repr__()
                         for x in BookSection.objects.filter(book=self.object)]
         context['section_data'] = section_data
+
         user_book = UserBook.objects.filter(user=self.request.user, book=self.object).first()
         if user_book is None:
             user_book = UserBook.objects.create(user=self.request.user, book=self.object)
         form = UserBookForm(user_book.__dict__)
         context['form'] = form
+
+        other_users_books = UserBook.objects.filter(book=self.object).exclude(user=self.request.user)
+        context['other_data'] = [(x.user.username or x.user.firstname + x.user.lastname, x.unit_price, x.quantity) for x in other_users_books]
         return context
 
     def post(self, request, *args, **kwargs):
